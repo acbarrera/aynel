1652184b1c6c9aa75d2e47b193fe1ad94c56d29f
acbarr <acbarrera14@gmail.com>
2020-09-03 22:26:07 -0300

    del practico 10 ejercicio3

--- Practico9/src/test/java/practico10/practico10.java
+++ Practico9/src/test/java/practico10/practico10.java
@@ -10,7 +10,7 @@ import utilities.GetProperties;
 public class practico10 {
     private WebDriver driver;
 
-    @Test //ejercicio1
+    @Test
     public WebDriver abrirNavegador(String url){
         GetProperties properties = new GetProperties();
         String chromeDriverURL = properties.getString("CHROMEDRIVER_PATH");
@@ -22,23 +22,18 @@ public class practico10 {
         return driver;
         //driver.close();
     }
-    @Test
+    @Test //ejercicio1
     public void forgotAccount(){
         driver = abrirNavegador("https://www.facebook.com/");
         driver.findElement(By.linkText("Forgot account?")).click();
     }
-    @Test
+    @Test //ejercicio 2
     public void forgotAccountPartialLink(){
         driver = abrirNavegador("https://www.facebook.com/");
         driver.findElement(By.partialLinkText("Forgot ")).click();
     }
-    /*Crear un método de test llamado customSalesforceLink
-Acceder a: https://login.salesforce.com/
-Hacer click en el link “User a Custom Domain”
-Completar el custom domain con “as”
-Hacer click en boton Continue
-Volver atras*/
-    @Test
+
+    @Test //ejercicio 3
     public void customSalesforceLink(){
         driver = abrirNavegador("https://login.salesforce.com/");
         driver.findElement(By.linkText("Use Custom Domain")).click();
@@ -46,4 +41,5 @@ Volver atras*/
         driver.findElement(By.id("mydomainContinue")).click();
         driver.navigate().back();
     }
+  
 }
